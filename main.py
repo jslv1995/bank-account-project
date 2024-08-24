b4f6098fb8bd72210acdaf15f8a4f107c73df55e
John Lewis <jslv1995@gmail.com>
2024-08-24 18:50:54 -0400

    Refactor Bank Account and Checking/Savings Account

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 
-class BankAccount():
-    """A bank account that tracks user's available balances and transfers."""
+class Bank():
+    """Create a Bank account."""
+    account_number = 1
 
-    def __init__(self, account_owner, balance=0, account_type='checking'):
+    def __init__(self, account_owner):
         """Initialize the attributes of a bank account."""
         self.account_owner = account_owner
-        self.balance = balance
-        self.account_type = account_type
+        # Give the client an account number, increment by one.
+        self.account_number = Bank.account_number
+        Bank.account_number += 1 
         self.transaction_history = []
-
+    
     def get_account_owner(self):
         """Return the account owner's name."""
         return self.account_owner.title()
+    
+    def get_account_number(self):
+        """Return the account number."""
+        return self.account_number
 
     def get_balance(self):
         """Check current balance."""
         return f"Current balance: ${self.balance:.2f}."
-        
-    def get_transaction_history(self):
+    
+    def show_transaction_history(self):
         """Return the list of transactions."""
-        return f"Settled transactions: {self.transaction_history}"
+        return f"Settled transactions: {self.transaction_history}"    
+
+class CheckingAccount(Bank):
+    """Allow the owner to transfer and spend funds."""
+    def __init__(self, account_owner, balance=0):
+        """Initialize the Checking Account with transfer and spending capabilities."""
+        super().__init__(account_owner)
+        self.account_type = "Checking"
+        self.balance = balance
 
     def deposit(self, requested_deposit_amount):
         """Deposit a cash amount, must be positive."""
@@ -37,31 +51,33 @@ class BankAccount():
     def withdraw(self, requested_withdrawal_amount):
         """Withdraw a cash amount, no more than current balance."""
         try:
-            if requested_withdrawal_amount <= self.balance:
+            if requested_withdrawal_amount <= 0:
+                return "Error: Withdrawal amount must be greater than $0.00"             
+            elif requested_withdrawal_amount <= self.balance:
                 self.balance -= requested_withdrawal_amount
                 self.transaction_history.append(f"Withdrawal: {requested_withdrawal_amount * -1}")
                 return f"Please take your cash. Your updated balance is ${self.balance:.2f}."
             else:
                 return "Error: Insufficient funds."
-            
+                 
         except (TypeError, ValueError):
             return "Please enter a valid number"
         
-class SavingsAccount(BankAccount):
-    """Allows the owner to earn interest."""
-
-    def __init__(self, account_owner, balance=0, account_type='savings'):
-        """Initialize the savings account with 5% APY."""
-        # Inherit the Bank Account attributes.
-        super().__init__(account_owner, balance, account_type)
-        transaction_history = []
-        self.interest_rate = 0.05
+class SavingsAccount(Bank):
+    """Allow the owner to earn 5% APY on deposited funds."""
+    def __init__(self, account_owner, balance=0):
+        """Initialize the Savings Account with 5% APY."""
+        # Inherit the Bank attributes.
+        super().__init__(account_owner)
+        self.account_type = "Savings"
+        self.balance = balance
+        self.interest_rate = 0.05 #5% APY.
 
     def pay_monthly_interest(self):
         """Credit the account monthly interest."""
 
         # Calculate monthly rate and amount.
-        monthly_interest_rate = float(self.interest_rate/12)
+        monthly_interest_rate = float(self.interest_rate/12) 
         monthly_interest_paid = round(self.balance * monthly_interest_rate,2)
 
         # Credit the monthly amount of interest.
--- test_bank_account.py
+++ test_bank_account.py
@@ -1,43 +1,60 @@
 import pytest
-from main import BankAccount, SavingsAccount
+from main import Bank, CheckingAccount, SavingsAccount
 
-## Test Bank Account
-# Test Transaction History on each line
-
-def test_get_owners_name():
-    account = BankAccount("frankie knuckles")
+## Test Bank: Parent Class
+def test_get_bank_account_owners_name():
+    account = Bank("frankie knuckles")
     result = account.get_account_owner()
     assert result == "Frankie Knuckles"
 
-def test_get_balance():
-    account = BankAccount("frankie knuckles",100)
+def test_get_bank_account_number():
+    Bank.account_number = 1 # Reset to ensure consistent test results.
+    account = Bank("frankie knuckles")
+    result = account.get_account_number()
+    assert result == 2
+
+def test_get_checking_balance():
+    account = CheckingAccount("frankie knuckles", 100)
+    result = account.get_balance()
+    assert account.balance == 100
+    assert result == "Current balance: $100.00."
+
+def test_get_savings_balance():
+    account = SavingsAccount("frankie knuckles", 100)
     result = account.get_balance()
     assert account.balance == 100
     assert result == "Current balance: $100.00."
 
 # Tests deposit and the update to transaction history.
 def test_deposit():
-    account = BankAccount("frankie knuckles",100)
+    account = CheckingAccount("frankie knuckles", 100)
     result = account.deposit(50)
     assert account.balance == 150
     assert result == "Thank you for your deposit. Your updated balance is $150.00."
     assert account.transaction_history[-1] == "Deposit: 50"
 
 def test_negative_deposit():
-    account = BankAccount("frankie knuckles",100)
+    account = CheckingAccount("frankie knuckles", 100)
     result = account.deposit(-25)
     assert account.balance == 100
+    assert result == "Error: Unable to deposit negative amount."
 
 # Test withdrawal and the update to transaction history.
-def test_withdraw():
-    account = BankAccount("frankie knuckles",100)
+def test_negative_withdrawal():
+    account = CheckingAccount("frankie knuckles", 100)
+    result = account.withdraw(-50)
+    assert account.balance == 100
+    assert result == "Error: Withdrawal amount must be greater than $0.00"
+
+def test_withdrawal():
+    account = CheckingAccount("frankie knuckles", 100)
     result = account.withdraw(50)
     assert account.balance == 50
     assert result == "Please take your cash. Your updated balance is $50.00."
     assert account.transaction_history[-1] == "Withdrawal: -50"
 
 def test_exceeding_withdrawal():
-    account = BankAccount("frankie knuckles",100)
+    account = CheckingAccount("frankie knuckles", 100)
     result = account.withdraw(500)
     assert account.balance == 100
     assert result == "Error: Insufficient funds."
@@ -46,9 +63,10 @@ def test_exceeding_withdrawal():
 
 # Tests interest payment amount, updated account balance, and updated transaction history.
 def test_interest_payment():
-    account = SavingsAccount("frankie knuckles",100)
+    account = SavingsAccount("frankie knuckles", 100)
     result = account.pay_monthly_interest()
     expected_interest = round(100 * (0.05/12) ,2) # $100 balance, times monthly interest payment at 5% APY
     assert account.balance == 100 + expected_interest
     assert result == f"You've earned ${expected_interest} in interest this month!"
     assert account.transaction_history[-1] == f"Interest paid: ${expected_interest}"
+
